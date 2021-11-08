4c5b600248d27cf60c1d065592a13c3726ec6a9a
Acer <alibektursunov70@gmail.com>
2021-11-08 10:50:39 +0500

    men 3 chi oynani qo'shdim

---
+++ lib/pags/page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class page extends StatefulWidget {
+  const page({Key? key}) : super(key: key);
+
+  @override
+  _pageState createState() => _pageState();
+}
+
+class _pageState extends State<page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('bu 4chi oyna'),),);
+  }
+}

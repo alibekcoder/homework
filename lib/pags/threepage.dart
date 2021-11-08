1adab00904a3316fe3db742e12f617b9f1d687f1
Acer <alibektursunov70@gmail.com>
2021-11-08 10:42:48 +0500

    men 3 chi oynani qo'shdim

---
+++ lib/pags/threepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class threePage extends StatefulWidget {
+  const threePage({Key? key}) : super(key: key);
+
+  @override
+  _threePageState createState() => _threePageState();
+}
+
+class _threePageState extends State<threePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child:Text("Bu 3 chi oyna")),);
+  }
+}
